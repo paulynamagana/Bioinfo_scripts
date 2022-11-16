a94b8f4b386457f6d1d555d18a03c0cde928a9c0
Paulyna Magana <paxpm8@nottingham.ac.uk>
2022-11-16 16:46:10 +0000

    Initial commit

---
+++ DNAtoRNA.py
@@ -0,0 +1,32 @@
+# define a function for transcription
+def DNAtoRNA(seq):
+
+    # convert string into list
+    l = list(seq)
+
+    for i in range(len(seq)):
+
+        if (l[i] == "T"):
+            l[i] = "A"
+
+        elif (l[i] == "G"):
+            l[i] = "C"
+
+        elif (l[i] == "C"):
+            l[i] = "G"
+
+        elif (l[i] == "A"):
+            l[i] = "U"
+
+        else:
+            print("Invalid")
+
+    print("Translated DNA; ", end="")
+    for char in l:
+        print(char, end="")
+
+
+if __name__ == "__main__":
+
+    y = input("DNA sequence: ")
+    DNAtoRNA(y)
